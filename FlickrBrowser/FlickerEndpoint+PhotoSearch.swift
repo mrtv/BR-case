87e3387ac3d04ea1958b33daea60385a9eac0227
Murat Tezyapar <murat.tezyapar@live.com>
2023-03-07 06:09:07 +0100

    Add page size and index to service call to support pagination

--- FlickrBrowser/FlickerEndpoint+PhotoSearch.swift
+++ FlickrBrowser/FlickerEndpoint+PhotoSearch.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 extension FlickrEndpoint {
-    static func search(query: String) -> FlickrEndpoint {
+    static func search(query: String, pageSize: Int = 25, page: Int = 0) -> FlickrEndpoint {
         return FlickrEndpoint(
             method: "flickr.photos.search",
             queryItems: [
-                URLQueryItem(name: "text", value: query)
+                URLQueryItem(name: "text", value: query),
+                URLQueryItem(name: "per_page", value: "\(pageSize)"),
+                URLQueryItem(name: "page", value: "\(page)")
             ]
         )
     }
