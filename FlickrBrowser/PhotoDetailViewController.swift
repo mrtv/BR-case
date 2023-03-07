b631decd0ae8e30a34c543b45f16c7977a8680ce
Murat Tezyapar <murat.tezyapar@live.com>
2023-03-07 05:32:38 +0100

    Bind PhotoDetail scene display elements

--- FlickrBrowser/PhotoDetailViewController.swift
+++ FlickrBrowser/PhotoDetailViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class PhotoDetailViewController: UIViewController {
     internal var viewModel: PhotoDetailViewModel!
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.loadData()
+    }
+    
+    func loadData() {
+        self.title = viewModel.title
+        self.loadImage()
     }
 
+    func loadImage() {
+        if let url = viewModel.imageURL {
+            DispatchQueue.global().async {
+                do {
+                    let data = try Data(contentsOf: url)
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    }
+                } catch {
+                    self.imageView.image = nil
+                }
+            }
+        }
+    }
 }
